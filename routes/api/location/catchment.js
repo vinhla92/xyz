29c0712361a7567b407280c9206b63d9074c0915
Rob Hurst <robert.hurst@geolytix.co.uk>
2018-11-02 16:38:05 +0200

    eslint

--- routes/api/location/catchment.js
+++ routes/api/location/catchment.js
@@ -1,10 +1,10 @@
 module.exports = fastify => {
-    fastify.route({
-      method: 'POST',
-      url: '/api/location/catchment',
-      beforeHandler: fastify.auth([fastify.authAPI]),
-      handler: (req, res) => {
-        require(global.appRoot + '/mod/catchment').catchment(req, res, fastify);
-      }
-    });
+  fastify.route({
+    method: 'POST',
+    url: '/api/location/catchment',
+    beforeHandler: fastify.auth([fastify.authAPI]),
+    handler: (req, res) => {
+      require(global.appRoot + '/mod/catchment').catchment(req, res, fastify);
+    }
+  });
 };
