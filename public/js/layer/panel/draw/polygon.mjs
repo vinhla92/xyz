9253bd7a16048c6aee107f48aec401e753529308
Agata Brok <agata.brok@geolytix.co.uk>
2018-11-06 15:11:43 +0100

    Fixed polygon editing.

--- public/js/layer/panel/draw/polygon.mjs
+++ public/js/layer/panel/draw/polygon.mjs
@@ -1,8 +1,9 @@
 import _xyz from '../../../_xyz.mjs';
 import style from './style.mjs';
+import { finish } from './_draw.mjs';
 //import { booleanValid } from '@turf/boolean-valid';
 
-export function polygon (layer) {
+export function polygon (e, layer) {
 
   if(!layer.display) layer.show();
 
@@ -56,6 +57,71 @@ export function polygon (layer) {
       ], style(layer).path));
 
     });
+
+    _xyz.map.on('contextmenu', e => {
+                
+      _xyz.map.off('mousemove');
+      _xyz.map.off('contextmenu');
+      _xyz.map.off('click');
+      _xyz.dom.map.style.cursor = '';
+
+      layer.edit.trail.clearLayers();
+      
+      coords = [];
+      layer.edit.vertices.eachLayer(layer => {
+          coords.push([layer.getLatLng().lng, layer.getLatLng().lat]);
+      });
+      coords.push(coords[0]);
+      
+      let poly = {
+          "type": "Polygon",
+          "coordinates": [coords],
+          "properties": {}
+      };
+      
+      // Make select tab active on mobile device.
+      if (_xyz.view.mobile) _xyz.view.mobile.activateLayersTab();
+      
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', _xyz.host + '/api/location/new?token=' + _xyz.token);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      
+      let _marker = layer.edit.vertices.getBounds().getCenter(),
+          marker = [_marker.lng.toFixed(5), _marker.lat.toFixed(5)];
+          
+      xhr.onload = e => {
+          if (e.target.status === 401) {
+              document.getElementById('timeout_mask').style.display = 'block';
+              //console.log(e.target.response);
+              return
+          }
+          
+          if (e.target.status === 200) {
+              layer.edit.vertices.clearLayers();
+              layer.edit.path.clearLayers();
+              
+              layer.get();
+
+              finish(layer);
+              _xyz.state.finish();
+              
+              _xyz.locations.select({
+                  layer: layer.key,
+                  table: layer.table,
+                  id: e.target.response,
+                  marker: marker,
+                  editable: layer.edit ? layer.edit.properties : false
+              });
+          }
+      }
+      
+      xhr.send(JSON.stringify({
+          locale: _xyz.locale,
+          layer: layer.key,
+          table: layer.table,
+          geometry: poly
+      }));
+    }); 
 
   });
 
