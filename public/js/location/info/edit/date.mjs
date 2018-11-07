46c7ad19614c9b60fb77dfa4a5ee279ef034de6f
Agata Brok <agata.brok@geolytix.co.uk>
2018-11-07 12:17:49 +0100

    Date formatting moved to date module.

--- public/js/location/info/edit/date.mjs
+++ public/js/location/info/edit/date.mjs
@@ -4,6 +4,34 @@ import valChange from './valChange.mjs';
 
 export default (record, entry) => {
 
-  console.log(entry);
+  console.log(record);
+  //console.log(entry);
+  
+   if(entry.type === 'datetime'){
+     entry.value = formatDateTime(rows[0][entry.field]); 
+     return;
+    }
+    
+    if(entry.type === 'date'){
+      entry.value = formatDate(rows[0][entry.field]);
+      console.log(entry.value);
+      return;
+    }
 
 };
+
+export function formatDate(str){
+  console.log(str);
+  let d = new Date(str),
+    options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' },
+    loc = 'en-GB';
+    console.log(d);
+  return d ? d.toLocaleDateString(loc, options) : false;
+}
+
+export function formatDateTime(str){
+  let d = new Date(str),
+    options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' },
+    loc = 'en-GB';
+  return d ? d.toLocaleDateString(loc, options) + ', ' + d.toLocaleTimeString(loc) : false;
+}
