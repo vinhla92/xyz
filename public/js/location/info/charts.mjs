2b8e48f7197f7ef81b4477abbc7c52be5a4d76b3
Agata Brok <agata.brok@geolytix.co.uk>
2018-11-16 09:25:06 +0000

    Test commit after node upgrade.

--- public/js/location/info/charts.mjs
+++ public/js/location/info/charts.mjs
@@ -11,43 +11,44 @@ export default (group) => {
     tag: 'canvas',
     appendTo: div
   });
+  
 
   //console.log(group);
   //console.log(JSON.stringify(group));
 
-  let labels = group.fields.map(field => {return field.label}),
-      data = group.fields.map(field => {return field.value}),
+  let labels = group.fields.map(field => {return field.label;}),
+    data = group.fields.map(field => {return field.value;}),
       
-      _chart = new Chart(canvas, {
-        type: group.chart.type || 'line',
-        data: {
-          labels: labels,
-          datasets: [{
-            label: group.label,
-                backgroundColor: group.chart.backgroundColor || '#cf9',
-                borderColor: group.chart.borderColor || '#079e00',
-                data: data
-            }]
-        },
-        options: {
-            responsive: true,
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    callback: (label, index, labels) => {
-                      return label/1000 + 'k';
-                    }
-                  },
-                  scaleLabel: {
-                    display: true,
-                    labelString: '1k = 1000'
-                  }
+    _chart = new Chart(canvas, {
+      type: group.chart.type || 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: group.label,
+          backgroundColor: group.chart.backgroundColor || '#cf9',
+          borderColor: group.chart.borderColor || '#079e00',
+          data: data
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                callback: (label, index, labels) => {
+                  return label/1000 + 'k';
                 }
-              ]
+              },
+              scaleLabel: {
+                display: true,
+                labelString: '1k = 1000'
+              }
             }
+          ]
         }
+      }
     });
-    return div;
-    //console.log(canvas);
+  return div;
+  //console.log(canvas);
 };
