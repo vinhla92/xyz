c0700f748ce2ba06e5a133db6220587e15c590cb
Agata Brok <agatbr@gmail.com>
2019-11-06 14:20:18 +0000

    Fix to cluster popup - highlight on hover.

--- public/js/xyz_openlayers/layer/selectCluster.mjs
+++ public/js/xyz_openlayers/layer/selectCluster.mjs
@@ -36,7 +36,7 @@ export default _xyz => function(feature) {
 
     if (cluster.length > 1) {
 
-      const ul = _xyz.utils.wire()`<ul class="scroll-list secondary-colour-bg">`;
+      const ul = _xyz.utils.wire()`<ul class="scroll-list">`;
 
       cluster.forEach(li => {
 
